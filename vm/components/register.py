71129d860587440c5110a73ad46b9f2e7fb16d4e
H4rtland <h4rtland@gmail.com>
2017-05-08 21:37:05 +0100

    Clean up register code, fix overflow/underflow off by one error

--- vm/components/register.py
+++ vm/components/register.py
@@ -9,7 +9,7 @@ class Register:
 
     def load(self, value):
         if not (isinstance(value, int) or isinstance(value, Register)):
-            raise RegisterException(f"Register {self.name} set to non integer value ({type(value).__name__}: {value})")
+            raise RegisterException(f"Register {self.name} set to non integer value ({other.__class__.__name__}: {value})")
         if isinstance(value, Register):
             value = value.value
         if not 0 <= value <= 255:
@@ -20,56 +20,56 @@ class Register:
     def value(self):
         return self.stored_value
 
-    def __add__(self, register):
-        if not type(register) in (int, Register):
-            raise RegisterException(f"Addition wasn't an int or other register ({type(register).__name__}: {register})")
-        if type(register) is int:
-            self.stored_value += register
-        elif type(register) is Register:
-            self.stored_value += register.value
+    def __add__(self, other):
+        if not isinstance(other, (int, Register)):
+            raise RegisterException(f"Addition wasn't an int or other register ({other.__class__.__name__}: {other})")
+        if isinstance(other, int):
+            self.stored_value += other
+        elif isinstance(other, Register):
+            self.stored_value += other.value
         
         if self.stored_value > 255:
-            self.stored_value %= 255
+            self.stored_value %= 256
         elif self.stored_value < 0:
-            self.stored_value = (self.stored_value % 255) + 1
+            self.stored_value = (self.stored_value % 256)
         return self
 
     def __sub__(self, register):
         return self.__add__(-register)
 
     def __neg__(self):
-        self.load((-self.stored_value & 0b11111111) - 1)
+        self.load((-self.stored_value & 0b11111111))
         return self
     
     def __mul__(self, other):
-        if not type(other) in (int, Register):
-            raise RegisterException(f"Addition wasn't an int or other register ({type(other).__name__}: {other})")
-        if type(other) is int:
+        if not isinstance(other, (int, Register)):
+            raise RegisterException(f"Addition wasn't an int or other register ({other.__class__.__name__}: {other})")
+        if isinstance(other, int):
             self.stored_value *= other
-        elif type(other) is Register:
+        elif isinstance(other, Register):
             self.stored_value *= other.value
         
         if self.stored_value > 255:
-            self.stored_value %= 255
+            self.stored_value %= 256
         elif self.stored_value < 0:
-            self.stored_value = (self.stored_value % 255) + 1
+            self.stored_value = (self.stored_value % 256)
         return self
     
     def __truediv__(self, other):
         self.__floordiv__(other)
     
     def __floordiv__(self, other):
-        if not type(other) in (int, Register):
-            raise RegisterException(f"Addition wasn't an int or other register ({type(other).__name__}: {other})")
-        if type(other) is int:
+        if not isinstance(other, (int, Register)):
+            raise RegisterException(f"Addition wasn't an int or other register ({other.__class__.__name__}: {other})")
+        if isinstance(other, int):
             self.stored_value //= other
-        elif type(other) is Register:
+        elif isinstance(other, Register):
             self.stored_value //= other.value
         
         if self.stored_value > 255:
-            self.stored_value %= 255
+            self.stored_value %= 256
         elif self.stored_value < 0:
-            self.stored_value = (self.stored_value % 255) + 1
+            self.stored_value = (self.stored_value % 256)
         return self
         
     
